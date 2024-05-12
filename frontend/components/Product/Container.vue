4d7a5fd2a15c0f768af923e7edb936e4d437fb6f
informationtechnologystore <itechstore.com.np@gmail.com>
2024-05-12 14:35:37 +0545

    responsive

--- frontend/components/Product/Container.vue
+++ frontend/components/Product/Container.vue
@@ -8,7 +8,7 @@ const { text } = toRefs(props);
 <template>
   <h1 class="text-xl pt-8 font-semibold">{{ text }}</h1>
   <section
-    class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-10 gap-x-8 mt-10 mb-5"
+    class="w-fit mx-auto grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-10 gap-x-8 mt-10 mb-5"
   >
     <slot></slot>
   </section>
